eeb4441f1e097fe815fab3f8a3ba838d64c53a29
edmondgaks <edmondgaks@gmail.com>
2024-03-29 20:00:03 +0200

    Placeholders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,22 +110,22 @@ export default function Home() {
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 p-2 gap-2">
-        <Image src={"/image1.png"} width={320} height={320} alt="image" />
-        <Image src={"/image2.png"} width={320} height={320} alt="image" />
-        <Image src={"/image3.png"} width={320} height={320} alt="image" />
-        <Image src={"/image4.png"} width={320} height={320} alt="image" />
-        <Image src={"/image5.png"} width={320} height={320} alt="image" />
-        <Image src={"/image6.png"} width={320} height={320} alt="image" />
-        <Image src={"/image7.png"} width={320} height={320} alt="image" />
+        <Image src={"/image1.png"} width={320} height={320} alt="image1" />
+        <Image src={"/image2.png"} width={320} height={320} alt="image2" />
+        <Image src={"/image3.png"} width={320} height={320} alt="image3" />
+        <Image src={"/image4.png"} width={320} height={320} alt="image4" />
+        <Image src={"/image5.png"} width={320} height={320} alt="image5" />
+        <Image src={"/image6.png"} width={320} height={320} alt="image6" />
+        <Image src={"/image7.png"} width={320} height={320} alt="image7" />
         <Image src={"/image8.png"} width={320} height={320} alt="image" />
-        <Image src={"/image9.png"} width={320} height={320} alt="image" />
-        <Image src={"/image10.png"} width={320} height={320} alt="image" />
-        <Image src={"/image11.png"} width={320} height={320} alt="image" />
-        <Image src={"/image12.png"} width={320} height={320} alt="image" />
-        <Image src={"/image13.png"} width={320} height={320} alt="image" />
-        <Image src={"/image14.png"} width={320} height={320} alt="image" />
-        <Image src={"/image15.png"} width={320} height={320} alt="image" />
-        <Image src={"/image16.png"} width={320} height={320} alt="image" />
+        <Image src={"/image9.png"} width={320} height={320} alt="image9" />
+        <Image src={"/image10.png"} width={320} height={320} alt="image10" />
+        <Image src={"/image11.png"} width={320} height={320} alt="image11" />
+        <Image src={"/image12.png"} width={320} height={320} alt="image12" />
+        <Image src={"/image13.png"} width={320} height={320} alt="image13" />
+        <Image src={"/image14.png"} width={320} height={320} alt="image14" />
+        <Image src={"/image15.png"} width={320} height={320} alt="image15" />
+        <Image src={"/image16.png"} width={320} height={320} alt="image16" />
       </div>
       <div className="h-[80vh] p-2 gap-10 flex flex-col bg-[#E4E4E4]">
         <div className="w-[50%] text-black flex flex-row justify-between">
